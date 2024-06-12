85078017750e60d9c35543e76d518d7d51bef54e
Lara Ozyegen <lara.ozyegen@gmail.com>
2024-06-12 16:36:33 +0000

    bertclinical training fixed

--- bert_crf_trainer.py
+++ bert_crf_trainer.py
@@ -1,16 +1,18 @@
 from datasets import load_dataset
 from sklearn.metrics import classification_report, f1_score, confusion_matrix, ConfusionMatrixDisplay
-from transformers import AutoTokenizer, BertTokenizerFast, Trainer, TrainingArguments, DataCollatorForTokenClassification
+from transformers import BertConfig, AutoTokenizer, BertTokenizerFast, Trainer, TrainingArguments, DataCollatorForTokenClassification
 from transformers.trainer_utils import IntervalStrategy
 import numpy as np
 import json
 import matplotlib.pyplot as plt
 from models import BertCRF
 import pandas as pd
+import os
 
 #
 
-
+# model_checkpoints = ['allenai/scibert_scivocab_uncased', 'bionlp/bluebert_pubmed_uncased_L-24_H-1024_A-16', 'google-bert/bert-base-uncased', 'samrawal/bert-base-uncased_clinical-ner' ,'emilyalsentzer/Bio_ClinicalBERT', 'microsoft/deberta-base']
+# 
 # Split the dataset manually
 # train_test_split = full_dataset['train'].select(range(20)).train_test_split(test_size=0.2)
 # train_dataset = train_test_split['train']
@@ -19,6 +21,7 @@ import pandas as pd
 # train_dataset, test_dataset = load_dataset('conll2003', split=['train', 'test'])
 
 model_checkpoint = 'samrawal/bert-base-uncased_clinical-ner'
+model_name = 'bertclinical-ft'
 
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 # data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
@@ -91,64 +94,145 @@ def untokenize_labels_predictions(word_ids, true_labels, predictions):
 
     return untokenized_true_labels, untokenized_predictions
 class TrainingMonitor:
-  def __init__(self):
-      self.best_f1 = 0
-      self.best_confusion_matrix = None
-      
-  def compute_metrics_factory(self, word_ids, fold_no, dataset_name):
-    def compute_metrics(pred):
-      logits, labels = pred
-      predictions = np.argmax(logits, axis=-1)
+    def __init__(self):
+        self.best_f1 = 0
+        self.best_confusion_matrix = None
 
-      true_labels = [[id2label[l] for l in label[1:-1]] for label in labels]
-      true_predictions = [
-          [id2label[p] for (p, l) in zip(prediction[1:-1], label[1:-1])]
-          for prediction, label in zip(predictions, labels)
-      ]
-      untokenized_true_labels, untokenized_predictions = untokenize_labels_predictions(word_ids, true_labels, true_predictions)
-      
-      unflat_true = [label for seq in untokenized_true_labels for label in seq]
-      unflat_pred = [label for seq in untokenized_predictions for label in seq]
-      unreport = classification_report(y_pred=unflat_pred, y_true=unflat_true, output_dict=True)
-      unreport['macro_wo_O'] = {'precision': (unreport['I-Background']['precision'] + unreport['I-Other']['precision'] + unreport['I-Problem']['precision'] + unreport['I-Test']['precision'] + unreport['I-Treatment']['precision']) / 5,
-      'recall': (unreport['I-Background']['recall'] + unreport['I-Other']['recall'] + unreport['I-Problem']['recall'] + unreport['I-Test']['recall'] + unreport['I-Treatment']['recall']) / 5,
-      'f1-score': (unreport['I-Background']['f1-score'] + unreport['I-Other']['f1-score'] + unreport['I-Problem']['f1-score'] + unreport['I-Test']['f1-score'] + unreport['I-Treatment']['f1-score']) / 5,
-      'support': (unreport['I-Background']['support'] + unreport['I-Other']['support'] + unreport['I-Problem']['support'] + unreport['I-Test']['support'] + unreport['I-Treatment']['support'])}
-      
-      un_report_df = pd.DataFrame(unreport).round(3).T
+    def compute_metrics_factory(self, word_ids, fold_no, eval_dataset_name, model_name):
+        # Define the actual compute_metrics function
+        def compute_metrics(eval_preds):
+            logits, labels = eval_preds
+            predictions = np.argmax(logits, axis=-1)
 
-      new_f1_score = unreport['macro_wo_O']['f1-score']
-      if self.best_f1 < new_f1_score:
-          self.best_f1 = new_f1_score
-          cm = confusion_matrix(y_pred=unflat_pred, y_true=unflat_true)
-          disp = ConfusionMatrixDisplay(cm, display_labels=np.array(['I-Background','I-Other', 'I-Problem', 'I-Test', 'I-Treatment', 'O']))
-          fig, ax = plt.subplots(figsize=(8, 8))
-          disp.plot(ax=ax)
+            # Remove ignored index (special tokens) and convert to labels
+            true_labels = [[id2label[l] for l in label if l != -100] for label in labels]
+            true_predictions = [
+                [id2label[p] for (p, l) in zip(prediction, label) if l != -100]
+                for prediction, label in zip(predictions, labels)
+            ]
+
+            untokenized_true_labels, untokenized_predictions = untokenize_labels_predictions(word_ids, true_labels, true_predictions)
+
+            unflat_true = [label for seq in untokenized_true_labels for label in seq]
+            unflat_pred = [label for seq in untokenized_predictions for label in seq]
+            unreport = classification_report(y_pred=unflat_pred, y_true=unflat_true, output_dict=True)
+            unreport['macro_wo_O'] = {'precision': (unreport['I-Background']['precision'] + unreport['I-Other']['precision'] + unreport['I-Problem']['precision'] + unreport['I-Test']['precision'] + unreport['I-Treatment']['precision']) / 5,
+            'recall': (unreport['I-Background']['recall'] + unreport['I-Other']['recall'] + unreport['I-Problem']['recall'] + unreport['I-Test']['recall'] + unreport['I-Treatment']['recall']) / 5,
+            'f1-score': (unreport['I-Background']['f1-score'] + unreport['I-Other']['f1-score'] + unreport['I-Problem']['f1-score'] + unreport['I-Test']['f1-score'] + unreport['I-Treatment']['f1-score']) / 5,
+            'support': (unreport['I-Background']['support'] + unreport['I-Other']['support'] + unreport['I-Problem']['support'] + unreport['I-Test']['support'] + unreport['I-Treatment']['support'])}
+            
+            un_report_df = pd.DataFrame(unreport).round(3).T
+
+            new_f1_score = unreport['macro_wo_O']['f1-score']
+            if self.best_f1 < new_f1_score:
+                self.best_f1 = new_f1_score
+                cm = confusion_matrix(y_pred=unflat_pred, y_true=unflat_true)
+                disp = ConfusionMatrixDisplay(cm, display_labels=np.array(['I-Background','I-Other', 'I-Problem', 'I-Test', 'I-Treatment', 'O']))
+                fig, ax = plt.subplots(figsize=(8, 8))
+                disp.plot(ax=ax)
+
+               
+                binary_predictions = ['0' if label == 'O' else '1' for label in unflat_pred]
+                binary_labels = ['0' if label == 'O' else '1' for label in unflat_true]
+
+                # Generate a classification report
+                binary_classification_report = classification_report(y_true=binary_labels, y_pred=binary_predictions, target_names=['O', 'I'], digits=3, output_dict=True)
+                
+                # # Save the figure to an image file
+                # plt.savefig(f'analysis/{model_name}/{dataset_name}/graphs/fold{fold_no}/confusion_matrix.png')
+                # with open(f"analysis/{model_name}/{dataset_name}/reports/fold{fold_no}/multiclass_classification_report.json", "w") as f:
+                #     json.dump(un_report_df.to_dict(), f, indent=4)
+                # with open(f"analysis/{model_name}/{dataset_name}/reports/fold{fold_no}/binary_classification_report.json", "w") as f:
+                #     json.dump(binary_classification_report, f, indent=4)
+
+                # Define the directory paths
+                graph_dir = f'analysis/{model_name}/{eval_dataset_name}/graphs/fold{fold_no}'
+                report_dir = f'analysis/{model_name}/{eval_dataset_name}/reports/fold{fold_no}'
+
+                # Create directories if they do not exist
+                os.makedirs(graph_dir, exist_ok=True)
+                os.makedirs(report_dir, exist_ok=True)
+
+                # Save the confusion matrix as a PNG file
+                plt.savefig(f'{graph_dir}/confusion_matrix.png')
+                plt.close()
+
+                # Save the multiclass classification report as a JSON file
+                with open(f"{report_dir}/multiclass_classification_report.json", "w") as f:
+                    json.dump(un_report_df.to_dict(), f, indent=4)
+
+                # Save the binary classification report as a JSON file
+                with open(f"{report_dir}/binary_classification_report.json", "w") as f:
+                    json.dump(binary_classification_report, f, indent=4)
+                
+
+            return {
+                "precision": unreport['macro avg']['precision'],
+                "recall": unreport['macro avg']['recall'],
+                "f1": unreport['macro avg']['f1-score'],
+                "accuracy": unreport['accuracy'],
+            }
+        return compute_metrics
+
+# class TrainingMonitor:
+#   def __init__(self):
+#       self.best_f1 = 0
+#       self.best_confusion_matrix = None
+      
+#   def compute_metrics_factory(self, word_ids, fold_no, dataset_name, model_checkpoint):
+#     def compute_metrics(pred):
+#       logits, labels = pred
+#       predictions = np.argmax(logits, axis=-1)
+
+#       true_labels = [[id2label[l] for l in label[1:-1]] for label in labels]
+#       true_predictions = [
+#           [id2label[p] for (p, l) in zip(prediction[1:-1], label[1:-1])]
+#           for prediction, label in zip(predictions, labels)
+#       ]
+#       untokenized_true_labels, untokenized_predictions = untokenize_labels_predictions(word_ids, true_labels, true_predictions)
+      
+#       unflat_true = [label for seq in untokenized_true_labels for label in seq]
+#       unflat_pred = [label for seq in untokenized_predictions for label in seq]
+#       unreport = classification_report(y_pred=unflat_pred, y_true=unflat_true, output_dict=True)
+#       unreport['macro_wo_O'] = {'precision': (unreport['I-Background']['precision'] + unreport['I-Other']['precision'] + unreport['I-Problem']['precision'] + unreport['I-Test']['precision'] + unreport['I-Treatment']['precision']) / 5,
+#       'recall': (unreport['I-Background']['recall'] + unreport['I-Other']['recall'] + unreport['I-Problem']['recall'] + unreport['I-Test']['recall'] + unreport['I-Treatment']['recall']) / 5,
+#       'f1-score': (unreport['I-Background']['f1-score'] + unreport['I-Other']['f1-score'] + unreport['I-Problem']['f1-score'] + unreport['I-Test']['f1-score'] + unreport['I-Treatment']['f1-score']) / 5,
+#       'support': (unreport['I-Background']['support'] + unreport['I-Other']['support'] + unreport['I-Problem']['support'] + unreport['I-Test']['support'] + unreport['I-Treatment']['support'])}
+      
+#       un_report_df = pd.DataFrame(unreport).round(3).T
+
+#       new_f1_score = unreport['macro_wo_O']['f1-score']
+#       if self.best_f1 < new_f1_score:
+#           self.best_f1 = new_f1_score
+#           cm = confusion_matrix(y_pred=unflat_pred, y_true=unflat_true)
+#           disp = ConfusionMatrixDisplay(cm, display_labels=np.array(['I-Background','I-Other', 'I-Problem', 'I-Test', 'I-Treatment', 'O']))
+#           fig, ax = plt.subplots(figsize=(8, 8))
+#           disp.plot(ax=ax)
 
           
-          binary_predictions = ['0' if label == 'O' else '1' for label in unflat_pred]
-          binary_labels = ['0' if label == 'O' else '1' for label in unflat_true]
+#           binary_predictions = ['0' if label == 'O' else '1' for label in unflat_pred]
+#           binary_labels = ['0' if label == 'O' else '1' for label in unflat_true]
 
-          # Generate a classification report
-          binary_classification_report = classification_report(y_true=binary_labels, y_pred=binary_predictions, target_names=['O', 'I'], digits=3, output_dict=True)
+#           # Generate a classification report
+#           binary_classification_report = classification_report(y_true=binary_labels, y_pred=binary_predictions, target_names=['O', 'I'], digits=3, output_dict=True)
           
-          # Save the figure to an image file
-          plt.savefig(f'analysis/{dataset_name}/graphs/fold{fold_no}/confusion_matrix.png')
-          with open(f"analysis/{dataset_name}/reports/fold{fold_no}/multiclass_classification_report.json", "w") as f:
-              json.dump(un_report_df.to_dict(), f, indent=4)
-          with open(f"analysis/{dataset_name}/reports/fold{fold_no}/binary_classification_report.json", "w") as f:
-              json.dump(binary_classification_report, f, indent=4)
+#           # Save the figure to an image file
+#           plt.savefig(f'analysis/{dataset_name}/{model_checkpoint}/graphs/fold{fold_no}/confusion_matrix.png')
+#           with open(f"analysis/{dataset_name}/{model_checkpoint}/reports/fold{fold_no}/multiclass_classification_report.json", "w") as f:
+#               json.dump(un_report_df.to_dict(), f, indent=4)
+#           with open(f"analysis/{dataset_name}/{model_checkpoint}/reports/fold{fold_no}/binary_classification_report.json", "w") as f:
+#               json.dump(binary_classification_report, f, indent=4)
 
-      return {
-        "precision": unreport['macro_wo_O']['precision'],
-        "recall": unreport['macro_wo_O']['recall'],
-        "f1": unreport['macro_wo_O']['f1-score'],
-        "accuracy": unreport['accuracy']
-        }
-    return compute_metrics
+#       return {
+#         "precision": unreport['macro_wo_O']['precision'],
+#         "recall": unreport['macro_wo_O']['recall'],
+#         "f1": unreport['macro_wo_O']['f1-score'],
+#         "accuracy": unreport['accuracy']
+#         }
+#     return compute_metrics
 
 dataset_name = 'phee'
-for i in range(5):
+for i in range(1):
   dataset = load_dataset('json', field='data', data_files={'train': f'data/processed/{dataset_name}/fold{i}/train.json', 'test': f'data/processed/{dataset_name}/fold{i}/test.json'})
   train_dataset = dataset['train']
   test_dataset = dataset['test']
@@ -167,14 +251,17 @@ for i in range(5):
   train_dataset.set_format('torch', columns=['input_ids', 'token_type_ids', 'attention_mask', 'label_ids'])
   test_dataset.set_format('torch', columns=['input_ids', 'token_type_ids', 'attention_mask', 'label_ids'])
 
-  
-  model = BertCRF.from_pretrained(model_checkpoint, num_labels=6, ignore_mismatched_sizes=True)
+  num_labels=6
+  config = BertConfig.from_pretrained(model_checkpoint)
+  config.num_labels = num_labels
+  model = BertCRF.from_pretrained(model_checkpoint, config=config, ignore_mismatched_sizes=True)
+  # model = BertCRF.from_pretrained(model_checkpoint, num_labels=6, ignore_mismatched_sizes=True)
 
   monitor = TrainingMonitor()
   training_args = TrainingArguments(
       output_dir=f'models/bert-crf-ner/fold{i}',
       overwrite_output_dir=True,
-      num_train_epochs=5,
+      num_train_epochs=1,
       per_device_train_batch_size=4,
       per_device_eval_batch_size=4,
       evaluation_strategy="epoch",
@@ -190,7 +277,7 @@ for i in range(5):
   trainer = Trainer(
       model=model,
       args=training_args,
-      compute_metrics=monitor.compute_metrics_factory(test_word_ids, fold_no=i, dataset_name=dataset_name),
+      compute_metrics=monitor.compute_metrics_factory(test_word_ids, fold_no=i, eval_dataset_name=dataset_name, model_name = model_name),
       train_dataset=train_dataset,
       eval_dataset=test_dataset
   )
